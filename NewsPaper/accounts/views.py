a40bb8df26e522400521d8c5314a44baea9851bd
Artem Savchenko <valter.199669@gmail.com>
2024-12-30 17:35:14 +0300

    D5.6 Access rights (update)

--- NewsPaper/accounts/views.py
+++ NewsPaper/accounts/views.py
@@ -3,9 +3,16 @@ from django.contrib.auth.models import User
 from django.views.generic.edit import CreateView
 from .forms import SignUpForm
 
+from django.contrib.auth import logout as auth_logout
+from django.shortcuts import redirect
+
 
 class SignUp(CreateView):
     model = User
     form_class = SignUpForm
     success_url = '/accounts/login'
     template_name = 'registration/signup.html'
+
+def logout(request):
+    auth_logout(request)
+    return redirect(request.META['HTTP_REFERER'])
