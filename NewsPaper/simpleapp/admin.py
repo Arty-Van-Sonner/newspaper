fa0e80b3c4b641112c662bcb1c85382e048b02fd
Artem Savchenko <valter.199669@gmail.com>
2025-02-17 20:17:47 +0300

    New functions in news.admin.py

--- NewsPaper/simpleapp/admin.py
+++ NewsPaper/simpleapp/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from django.contrib import admin
 from .models import *
+ 
+# создаём новый класс для представления товаров в админке
+class ProductAdmin(admin.ModelAdmin):
+    # list_display — это список или кортеж со всеми полями, которые вы хотите видеть в таблице с товарами
+    list_display = tuple(('name', 'price',)) # генерируем список имён всех полей для более красивого отображения
+ 
+ 
+# Register your models here.
+admin.site.register(Product, ProductAdmin)
 
 # Register your models here.
 
 admin.site.register(Category)
-admin.site.register(Product)
 admin.site.register(Material)
 admin.site.register(ProductMaterial)
