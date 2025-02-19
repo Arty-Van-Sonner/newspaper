e70aa0e4d7c9ee34ad825a890b8680cd8874e78c
Artem Savchenko <valter.199669@gmail.com>
2025-02-19 19:55:09 +0300

    Added new functions to admin-panel

--- NewsPaper/simpleapp/admin.py
+++ NewsPaper/simpleapp/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import *
  
+
+# напишем уже знакомую нам функцию обнуления товара на складе
+def nullfy_quantity(modeladmin, request, queryset): # все аргументы уже должны быть вам знакомы, самые нужные из них это request — объект хранящий информацию о запросе и queryset — грубо говоря набор объектов, которых мы выделили галочками.
+    queryset.update(quantity=0)
+nullfy_quantity.short_description = 'Обнулить товары' # описание для более понятного представления в админ панеле задаётся, как будто это объект
+
 # создаём новый класс для представления товаров в админке
 # class ProductAdmin(admin.ModelAdmin):
 #     # list_display — это список или кортеж со всеми полями, которые вы хотите видеть в таблице с товарами
@@ -12,6 +18,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'on_stock') # оставляем только имя и цену товара
     list_filter = ('price', 'quantity', 'name') # добавляем примитивные фильтры в нашу админку
     search_fields = ('name', 'category__name') # тут всё очень похоже на фильтры из запросов в базу
+    actions = [nullfy_quantity] # добавляем действия в список
  
 # Register your models here.
 admin.site.register(Product, ProductAdmin)
